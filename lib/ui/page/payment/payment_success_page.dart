a3136720f547b5665e0857ee8ed2b2dacc854045
Pawan Kumar <pawan221b@gmail.com>
2018-10-16 12:09:23 +0530

    Payment Landscape Fixed

--- lib/ui/page/payment/payment_success_page.dart
+++ lib/ui/page/payment/payment_success_page.dart
@@ -71,54 +71,60 @@ class PaymentSuccessPageState extends State<PaymentSuccessPage> {
             ));
   }
 
-  successTicket() => Container(
-        width: double.infinity,
-        padding: const EdgeInsets.all(16.0),
-        child: Material(
-          elevation: 2.0,
-          borderRadius: BorderRadius.circular(4.0),
-          child: Padding(
+  successTicket() => Expanded(
+        child: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ProfileTile(
-                  title: "Thank You!",
-                  textColor: Colors.purple,
-                  subtitle: "Your transaction was successful",
-                ),
-                ListTile(
-                  title: Text("Date"),
-                  subtitle: Text("26 June 2018"),
-                  trailing: Text("11:00 AM"),
-                ),
-                ListTile(
-                  title: Text("Pawan Kumar"),
-                  subtitle: Text("mtechviral@gmail.com"),
-                  trailing: CircleAvatar(
-                    radius: 20.0,
-                    backgroundImage: NetworkImage(
-                        "https://avatars0.githubusercontent.com/u/12619420?s=460&v=4"),
-                  ),
-                ),
-                ListTile(
-                  title: Text("Amount"),
-                  subtitle: Text("\$299"),
-                  trailing: Text("Completed"),
-                ),
-                Card(
-                  elevation: 0.0,
-                  color: Colors.grey.shade300,
-                  child: ListTile(
-                    leading: Icon(
-                      FontAwesomeIcons.ccAmex,
-                      color: Colors.blue,
+            child: Material(
+              clipBehavior: Clip.antiAlias,
+              elevation: 2.0,
+              borderRadius: BorderRadius.circular(4.0),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    ProfileTile(
+                      title: "Thank You!",
+                      textColor: Colors.purple,
+                      subtitle: "Your transaction was successful",
                     ),
-                    title: Text("Credit/Debit Card"),
-                    subtitle: Text("Amex Card ending ***6"),
-                  ),
-                )
-              ],
+                    ListTile(
+                      title: Text("Date"),
+                      subtitle: Text("26 June 2018"),
+                      trailing: Text("11:00 AM"),
+                    ),
+                    ListTile(
+                      title: Text("Pawan Kumar"),
+                      subtitle: Text("mtechviral@gmail.com"),
+                      trailing: CircleAvatar(
+                        radius: 20.0,
+                        backgroundImage: NetworkImage(
+                            "https://avatars0.githubusercontent.com/u/12619420?s=460&v=4"),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text("Amount"),
+                      subtitle: Text("\$299"),
+                      trailing: Text("Completed"),
+                    ),
+                    Card(
+                      clipBehavior: Clip.antiAlias,
+                      elevation: 0.0,
+                      color: Colors.grey.shade300,
+                      child: ListTile(
+                        leading: Icon(
+                          FontAwesomeIcons.ccAmex,
+                          color: Colors.blue,
+                        ),
+                        title: Text("Credit/Debit Card"),
+                        subtitle: Text("Amex Card ending ***6"),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
         ),
