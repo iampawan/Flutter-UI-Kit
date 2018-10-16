b2147e36959b1f0b3818138237dbfbf749ef20f1
Pawan Kumar <pawan221b@gmail.com>
2018-10-16 12:15:41 +0530

    Product Detail Landscape Fixed

--- lib/ui/page/shopping/shopping_two/product_desc.dart
+++ lib/ui/page/shopping/shopping_two/product_desc.dart
@@ -7,106 +7,108 @@ class ProductDesc extends StatelessWidget {
   const ProductDesc({Key key, this.product}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return new Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: new ListTile(
-            title: new Text(
-              product.name,
-              style: new TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w700),
-            ),
-            subtitle: new Text(
-              product.brand,
-              style: new TextStyle(
-                  fontSize: 15.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.normal),
-            ),
-            trailing: new Text(product.price,
+    return SingleChildScrollView(
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: new ListTile(
+              title: new Text(
+                product.name,
                 style: new TextStyle(
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.yellow)),
+                    fontSize: 18.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700),
+              ),
+              subtitle: new Text(
+                product.brand,
+                style: new TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal),
+              ),
+              trailing: new Text(product.price,
+                  style: new TextStyle(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.yellow)),
+            ),
           ),
-        ),
-        new SizedBox(
-          height: 10.0,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 28.0),
-          child: new Text(
-            product.description,
-            style: TextStyle(color: Colors.white),
-            textAlign: TextAlign.start,
+          new SizedBox(
+            height: 10.0,
           ),
-        ),
-        new SizedBox(
-          height: 30.0,
-        ),
-        new Card(
-          clipBehavior: Clip.antiAlias,
-          shape: new RoundedRectangleBorder(
-              borderRadius: new BorderRadius.circular(8.0)),
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: new Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                new Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    new Text(
-                      "Your Size",
-                      style: TextStyle(fontWeight: FontWeight.w700),
-                    ),
-                    new RawChip(
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 28.0),
+            child: new Text(
+              product.description,
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.start,
+            ),
+          ),
+          new SizedBox(
+            height: 30.0,
+          ),
+          new Card(
+            clipBehavior: Clip.antiAlias,
+            shape: new RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(8.0)),
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  new Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      new Text(
+                        "Your Size",
+                        style: TextStyle(fontWeight: FontWeight.w700),
+                      ),
+                      new RawChip(
+                          label: new Text(
+                            "M",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          backgroundColor: Colors.cyan)
+                    ],
+                  ),
+                  new Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      new Text("Color",
+                          style: TextStyle(fontWeight: FontWeight.w700)),
+                      new RawChip(
                         label: new Text(
-                          "M",
+                          "Red Blue",
                           style: TextStyle(color: Colors.white),
                         ),
-                        backgroundColor: Colors.cyan)
-                  ],
-                ),
-                new Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    new Text("Color",
-                        style: TextStyle(fontWeight: FontWeight.w700)),
-                    new RawChip(
-                      label: new Text(
-                        "Red Blue",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      backgroundColor: Colors.red,
-                    )
-                  ],
-                ),
-                new Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    new Text("Product ID",
-                        style: TextStyle(fontWeight: FontWeight.w700)),
-                    new RawChip(
-                      label: new Text(
-                        "PQ1001",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      backgroundColor: Colors.green,
-                    )
-                  ],
-                )
-              ],
+                        backgroundColor: Colors.red,
+                      )
+                    ],
+                  ),
+                  new Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      new Text("Product ID",
+                          style: TextStyle(fontWeight: FontWeight.w700)),
+                      new RawChip(
+                        label: new Text(
+                          "PQ1001",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        backgroundColor: Colors.green,
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
