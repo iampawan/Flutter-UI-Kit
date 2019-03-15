c27599887d633db3d76de7d8462bc91a139edb4f
Pawan Kumar <pawan221b@gmail.com>
2019-03-15 23:16:27 +0530

    new gmail like floating action button added

--- lib/ui/page/timeline/timeline_two_page.dart
+++ lib/ui/page/timeline/timeline_two_page.dart
@@ -178,12 +178,51 @@ class TimelineTwoPageState extends State<TimelineTwoPage> {
               child: FloatingActionButton(
                 onPressed: () {},
                 backgroundColor: Colors.white,
-                child: Icon(
-                  Icons.add,
+                child: CustomPaint(
+                  child: Container(),
+                  foregroundPainter: FloatingPainter(),
                 ),
               ),
             ),
       ),
     );
   }
+}
+
+class FloatingPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint amberPaint = Paint()
+      ..color = Colors.amber
+      ..strokeWidth = 5;
+
+    Paint greenPaint = Paint()
+      ..color = Colors.green
+      ..strokeWidth = 5;
+
+    Paint bluePaint = Paint()
+      ..color = Colors.blue
+      ..strokeWidth = 5;
+
+    Paint redPaint = Paint()
+      ..color = Colors.red
+      ..strokeWidth = 5;
+
+    canvas.drawLine(Offset(size.width * 0.27, size.height * 0.5),
+        Offset(size.width * 0.5, size.height * 0.5), amberPaint);
+    canvas.drawLine(
+        Offset(size.width * 0.5, size.height * 0.5),
+        Offset(size.width * 0.5, size.height - (size.height * 0.27)),
+        greenPaint);
+    canvas.drawLine(Offset(size.width * 0.5, size.height * 0.5),
+        Offset(size.width - (size.width * 0.27), size.height * 0.5), bluePaint);
+    canvas.drawLine(Offset(size.width * 0.5, size.height * 0.5),
+        Offset(size.width * 0.5, size.height * 0.27), redPaint);
+  }
+
+  @override
+  bool shouldRepaint(FloatingPainter oldDelegate) => false;
+
+  @override
+  bool shouldRebuildSemantics(FloatingPainter oldDelegate) => false;
 }
